4124edfffed3c99accf7d10737954dc8c20fd528
Yun Chen <Yunchenfly@gmail.com>
2022-11-05 19:53:41 -0400

    commit tree 24a3fb2624ff98f86836f5ffda231de7f228876f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`)
+}
+
+saturdayFun();
+
+function mondayWork(work = "go to the office") {
+    return (`This Monday, I will ${work}.`)
+}
+
+mondayWork();
+
+function wrapAdjective(String, sign = "*") {
+    return function (word = "special") {
+        return `You are ${String}${word}${String}!`
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
